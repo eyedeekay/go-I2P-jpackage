815d9468373f7c26b92e2d730699c142e8ad2b44
idk <hankhill19580@gmail.com>
2022-02-17 16:15:43 -0500

    Add laucher commands

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package I2P
+
+import (
+	"os/exec"
+	"path/filepath"
+)
+
+func LinuxCommand(dir string) (*exec.Cmd, error) {
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		return nil, err
+	}
+	execPath := filepath.Join(dir, "I2P", "bin", "I2P")
+	return exec.Command(execPath), nil
+}
+
+func WindowsCommand(dir string) (*exec.Cmd, error) {
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		return nil, err
+	}
+	execPath := filepath.Join(dir, "I2P", "I2P.exe")
+	return exec.Command(execPath), nil
+}
