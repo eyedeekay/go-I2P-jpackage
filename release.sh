c5fb0838df13152e2c6316ad8de52cf87579fc61
idk <hankhill19580@gmail.com>
2022-05-19 00:16:26 -0400

    add continuous release config

--- release.sh
+++ release.sh
@@ -3,5 +3,6 @@
 if [ -f "$HOME/github-release-config.sh" ]; then
     . "$HOME/github-release-config.sh"
 fi
+cd "$HOME/go/src/github.com/eyedeekay/go-I2P-jpackage"
 git pull upstream main
 make clean jpackage release upload
