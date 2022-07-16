a3dd51f6995c9177d23a3e60a0a1917c627faef6
idk <hankhill19580@gmail.com>
2022-07-15 20:32:48 -0400

    pull on clean

--- clean.sh
+++ clean.sh
@@ -4,5 +4,6 @@ if [ -f "$HOME/github-release-config.sh" ]; then
     . "$HOME/github-release-config.sh"
 fi
 cd "$HOME/go/src/github.com/eyedeekay/go-I2P-jpackage"
+git pull --all; true
 git pull upstream main
 make clean
