2f9de9463ba17c6815fbfe7cf895c42cf5395a89
idk <hankhill19580@gmail.com>
2022-07-17 22:06:22 -0400

    try using the powershell library

--- make.go
+++ make.go
@@ -130,7 +130,7 @@ func ExportEnv(key, value string) error {
 	if err != nil {
 		return fmt.Errorf("ExportEnv: cmd.exe /C SETX %s", err)
 	}
-	sts := `[System.Environment]::SetEnvironmentVariable("` + key + `", $Env:` + key + ` + ";` + value + `'", [System.EnvironmentVariableTarget]::User)`
+	sts := fmt.Sprintf(`[System.Environment]::SetEnvironmentVariable("%s", $Env:%s + ";%s'", [System.EnvironmentVariableTarget]::User)`, key, key, value)
 	err, stv, errout := gosh.PowershellOutput(sts)
 	if err != nil {
 		log.Println("ExportEnv: gosh.PowershellOutput:", errout)
